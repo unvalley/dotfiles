879550b79028ea395726df4bee6d8420e2e7e1d0
unvalley <kirohi.code@gmail.com>
2022-06-17 14:54:31 +0900

    chore: init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,9 @@
+require("rc/base")
+-- ===============================
+-- require("rc/option")
+-- require("rc/display")
+-- require("rc/pluginlist")
+-- require("rc/mappings")
+
+-- im-select setting for Japanese
+-- autocmd InsertLeave * :silent !/usr/local/bin/im-select com.google.inputmethod.Japanese.Roman
---
+++ dot_config/nvim/lua/rc/base.lua
@@ -0,0 +1,22 @@
+vim.cmd([[ "language " .. os.getenv("LANG") ]])
+vim.o.langmenu = os.getenv("LANG")
+
+vim.cmd 'colorscheme ron'
+vim.o.encoding = "utf-8"
+vim.o.shell="/bin/fish"
+vim.o.filetype = true
+vim.o.number = true
+vim.o.ruler = true
+vim.o.wildmenu = true
+vim.o.showcmd = true
+vim.o.showmatch = true
+vim.o.cursorline = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.shiftwidth=4
+vim.o.tabstop=4
+vim.o.softtabstop=4
+vim.o.autoindent = true
