92ea32897247439e1a011bea5b6921b3e940ca2f
unvalley <kirohi.code@gmail.com>
2022-06-17 18:54:05 +0900

    chore: add nvim-tree

--- dot_config/nvim/lua/rc/plugins.lua
+++ dot_config/nvim/lua/rc/plugins.lua
@@ -4,4 +4,10 @@ return require("packer").startup(function()
 	use "wbthomason/packer.nvim"
 
 	use{'rhysd/git-messenger.vim', opt = true, cmd = {'GitMessenger'}}
+
+	use {'kyazdani42/nvim-tree.lua',
+		requires = {'kyazdani42/nvim-web-devicons', -- optional, for file icon},
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	}
+}
 end)
