10aa6f22fe18a844b33ebabb2650da5bcd4d4e7f
unvalley <kirohi.code@gmail.com>
2022-07-10 15:04:44 +0900

    chore: remove previous configs

--- dot_config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("rc/base")
-require("rc/plugins")
-require("rc/keymappings")
-
--- omit install and compile when setting plugins
-vim.cmd[[autocmd BufWritePost /lua/rc/plugins.lua | PackerCompile]]
-
--- im-select setting for Japanese
-vim.cmd[[autocmd InsertLeave * :silent !/usr/local/bin/im-select com.google.inputmethod.Japanese.Roman]]
