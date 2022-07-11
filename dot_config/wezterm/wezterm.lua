c263d57e225e5dce24448287f322f4feb30ade6d
unvalley <kirohi.code@gmail.com>
2022-07-11 19:40:56 +0900

    style: edit dashboard ascii and transparency of wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -56,7 +56,7 @@ local config = {
 	use_ime = true,
 	font_size = 14.0,
 	color_scheme = "Jellybeans",
-	window_background_opacity = 0.85,
+	window_background_opacity = 0.95,
 	window_background_tint = 0.0,
 	adjust_window_size_when_changing_font_size = false,
 
