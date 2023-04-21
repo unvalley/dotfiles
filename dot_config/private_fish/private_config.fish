fad82ce65723f36e357fea2dad003335b7899db2
unvalley <kirohi.code@gmail.com>
2023-04-21 23:30:15 +0900

    chore: update

--- dot_config/private_fish/private_config.fish
+++ dot_config/private_fish/private_config.fish
@@ -21,7 +21,7 @@ fish_add_path ~/.rustup/toolchains/nightly-aarch64-apple-darwin/bin
 # Go
 export GOENV_ROOT="$HOME/.goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
-eval (goenv init -)
+# eval (goenv init -)
 export PATH="$GOROOT/bin:$PATH"
 export PATH="$PATH:$GOPATH/bin"
 
