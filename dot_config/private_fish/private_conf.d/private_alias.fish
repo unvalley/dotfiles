8702a5a2d11e7ac833eca5eead9b3b0689d26e6b
unvalley <kirohi.code@gmail.com>
2023-07-01 19:38:08 +0900

    feat: use abbr instaed of alias

--- dot_config/private_fish/private_conf.d/private_alias.fish
+++ dot_config/private_fish/private_conf.d/private_alias.fish
@@ -1,87 +1,62 @@
-function check_installed
+function is_installed
     command -v $argv[1] >/dev/null
 end
 
-if check_installed bat
-    alias cat 'bat'
-end
-
-if check_installed rg
-    alias grep 'rg'
-end
-
-if check_installed zellij
-    alias zel 'zellij'
-end
-
-if check_installed code
-    alias c 'code'
-end
-
-if check_installed exa
-    alias e 'exa --icons --git'
-    alias l 'clear && ls'
-    alias ls 'exa --icons --git'
-    alias ea 'exa -a --icons --git'
-    alias la 'exa -a --icons --git'
-    alias ee 'exa -aahl --icons --git'
-    alias ll 'exa -aahl --icons --git'
-    alias et 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
-    alias lt 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
-    alias eta 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
-    alias tree 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
-    alias lta 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
-end
-
-if check_installed trash-put
-    alias rm 'trash-put'
-end
-
-if check_installed date
-    alias now 'date "+%Y-%m-%d %H:%M:%S" | pbcopy'
-end
-
-if check_installed fzf
-    alias f 'fzf'
-    alias vif 'vim $(fzf)'
-end
-
+abbr -a c 'bat'
+abbr -a cat 'bat'
+abbr -a g 'rg'
+abbr -a z 'zellij'
+abbr -a e 'exa --icons --git'
+abbr -a l 'clear; and ls'
+abbr -a ls 'exa --icons --git'
+abbr -a ea 'exa -a --icons --git'
+abbr -a la 'exa -a --icons --git'
+abbr -a ee 'exa -aahl --icons --git'
+abbr -a ll 'exa -aahl --icons --git'
+abbr -a et 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+abbr -a lt 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+abbr -a eta 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+abbr -a t 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+abbr -a lta 'exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+abbr -a rm 'trash-put'
+abbr -a n 'date "+%Y-%m-%d %H:%M:%S" | pbcopy'
+abbr -a f 'fzf'
 
 # Node.js
-alias ni="npm install"
-alias nrc="npm run local"
-alias nrcl="npm run local:loose"
-alias nrs="npm run start -s --"
-alias nrb="npm run build -s --"
-alias nrd="npm run dev"
-alias nrt="npm run test -s --"
-alias nrtw="npm run test:watch -s --"
-alias nrv="npm run validate -s --"
-alias rmn="rm -rf node_modules"
-alias flush-npm="rm -rf node_modules && npm i && say NPM is done"
-alias nicache="npm install --prefer-offline"
-alias nioff="npm install --offline"
+abbr -a ni "npm install"
+abbr -a nrc "npm run local"
+abbr -a nrcl "npm run local:loose"
+abbr -a nrs "npm run start -s --"
+abbr -a nrb "npm run build -s --"
+abbr -a nrd "npm run dev"
+abbr -a nrt "npm run test -s --"
+abbr -a nrtw "npm run test:watch -s --"
+abbr -a nrv "npm run validate -s --"
+abbr -a rmn "rm -rf node_modules"
+abbr -a fn "rm -rf node_modules; and npm i; and say NPM is done"
+abbr -a nic "npm install --prefer-offline"
+abbr -a nio "npm install --offline"
 
 
 # Git
-alias g='git'
-alias gi='git init'
-alias ga='git add'
-alias gaa='git add .'
-alias gaaa='git add -A'
-alias gb='git branch'
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gco='git checkout'
-alias gdf='git diff'
-alias gdfh='git diff HEAD'
-alias gl='git log'
-alias glg='git log --graph --oneline --decorate --all'
-alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
-alias gm='git merge --no-ff'
-alias gpl='git pull origin'
-alias gpo='git push origin'
-alias gst='git status'
-alias gss='git status -s'
-alias tasks='gh issue list --repo unvalley/projects'
+abbr -a g 'git'
+abbr -a gi 'git init'
+abbr -a ga 'git add'
+abbr -a gaa 'git add .'
+abbr -a gaaa 'git add -A'
+abbr -a gb 'git branch'
+abbr -a gc 'git commit'
+abbr -a gcm 'git commit -m'
+abbr -a gco 'git checkout'
+abbr -a gdf 'git diff'
+abbr -a gdfh 'git diff HEAD'
+abbr -a gl 'git log'
+abbr -a glg 'git log --graph --oneline --decorate --all'
+abbr -a gld 'git log --pretty=format:"%h %ad %s" --date=short --all'
+abbr -a gm 'git merge --no-ff'
+abbr -a gpl 'git pull origin'
+abbr -a gpo 'git push origin'
+abbr -a gst 'git status'
+abbr -a gss 'git status -s'
+abbr -a tasks 'gh issue list --repo unvalley/projects'
 
